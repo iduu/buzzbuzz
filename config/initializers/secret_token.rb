0ee790554184a10c02751ee5168e6ba56cdf4ddd
wsxiaoys <wsxiaoys.lh@gmail.com>
2011-05-16 09:30:32 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buzzbuzz::Application.config.secret_token = 'accc36f2f98975c1970f470d8eec295b7382d82fad33ab3637a79857692c537c291e50665461d32ac787d63b04e65959c5610075a1126e9a7c79852cc48ba768'
