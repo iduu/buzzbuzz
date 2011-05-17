4fa23a5b3b4a2cff60fccef0e4895862e3af896d
wsxiaoys <wsxiaoys.lh@gmail.com>
2011-05-17 08:51:16 +0800

    add user model
    add about to user

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,8 @@ module Buzzbuzz
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    # Set for email host
+    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   end
 end
