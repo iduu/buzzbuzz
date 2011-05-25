e19789cafe8db8c0d4b868c6d5a6707e26697a0b
wsxiaoys <wsxiaoys.lh@gmail.com>
2011-05-25 14:28:26 +0800

    do cleaning

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < Item
   # Validates
-  validates :title, :presence => true
+  validates :title, :presence => true,
                     :length => { maximum: 30 } 
   
   validates :url, :presence => true
